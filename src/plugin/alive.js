a457ab94c95600ea513e21c0d3a5668c2af7dd75
rushamd <schat5444@gmail.com>
2024-07-29 17:26:37 +0530

    Update alive.js

--- src/plugin/alive.js
+++ src/plugin/alive.js
@@ -36,7 +36,14 @@ _________________________________________
             display_text: "PING",
             id: `.ping`
           })
-        }
+                },
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "OWNER",
+            id: `.owner`
+          })
+}
         ];
 
   const msg = generateWAMessageFromContent(m.from, {
